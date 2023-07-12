013446160723cb727075fe2ecf6c8c37fe24f167
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2023-07-12 20:54:27 +0000

    Remove obsolete static routes

--- app.rb
+++ app.rb
@@ -5,7 +5,7 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dynamic/:number_of_dice/:how_many_sides") do
+get("/dice/:number_of_dice/:how_many_sides") do
   @num_dice = params.fetch("number_of_dice").to_i
 
   @sides = params.fetch("how_many_sides").to_i
@@ -19,45 +19,4 @@ get("/dynamic/:number_of_dice/:how_many_sides") do
   end
 
   erb(:flexible)
-end
-
-get("/dice/2/6") do
-  first_die = rand(1..6)
-	second_die = rand(1..6)
-  sum = first_die + second_die
-	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-	erb(:two_six)
-end
-
-get("/dice/2/10") do
-  first_die = rand(1..10)
-	second_die = rand(1..10)
-  sum = first_die + second_die
-	
-	@outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  erb(:two_ten)
-end
-
-get("/dice/1/20") do
-  @die = rand(1..20)
-	
-	@outcome = "You rolled a #{@die}."
-	
-	erb(:one_twenty)
-end
-
-get("/dice/5/4") do
-  first_die = rand(1..4)
-	second_die = rand(1..4)
-  third_die = rand(1..4)
-  fourth_die = rand(1..4)
-  fifth_die = rand(1..4)
-  sum = first_die + second_die + third_die + fourth_die + fifth_die
-	
-	@outcome = "You rolled a #{first_die}, #{second_die}, #{third_die}, #{fourth_die}, and #{fifth_die} for a total of #{sum}."
-	
-  erb(:five_four)
 end
