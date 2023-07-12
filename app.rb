fd95134dd72a076848c3814787eea0968e672665
Raghu Betina <Raghu.Betina@chicagobooth.edu>
2023-07-12 20:50:19 +0000

    Fully dynamic

--- app.rb
+++ app.rb
@@ -5,13 +5,15 @@ get("/") do
   erb(:homepage)
 end
 
-get("/dynamic/:number_of_dice/6") do
+get("/dynamic/:number_of_dice/:how_many_sides") do
   @num_dice = params.fetch("number_of_dice").to_i
+
+  @sides = params.fetch("how_many_sides").to_i
 
   @rolls = []
 
   @num_dice.times do
-    die = rand(1..6)
+    die = rand(1..@sides)
 
     @rolls.push(die)
   end
